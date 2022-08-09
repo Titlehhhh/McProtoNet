103c744f85d713305240bb85bd62678d80a0880d
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-08-09 12:48:16 +0500

    window packets

--- McProtoNet.Protocol754/Packets/Server/Game/ServerConfirmTransactionPacket.cs
+++ McProtoNet.Protocol754/Packets/Server/Game/ServerConfirmTransactionPacket.cs
@@ -4,13 +4,19 @@ namespace McProtoNet.Protocol754.Packets.Server
     [PacketInfo(0x11, PacketCategory.Game, 754, PacketSide.Server)]
     public sealed class ServerConfirmTransactionPacket : Packet
     {
+        public byte WindowId { get; private set; }
+        public ushort ActionId { get; private set; }
+        public bool Accepted { get; private set; }
+
         public override void Write(IMinecraftPrimitiveWriter stream)
         {
 
         }
         public override void Read(IMinecraftPrimitiveReader stream)
         {
-
+            WindowId = stream.ReadUnsignedByte();
+            ActionId = stream.ReadUnsignedShort();
+            Accepted = stream.ReadBoolean();
         }
         public ServerConfirmTransactionPacket() { }
     }
