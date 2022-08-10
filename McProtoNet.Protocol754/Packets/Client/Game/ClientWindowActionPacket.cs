4d0eecdc747e687381a8d012d055ad63ef8c9e67
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-08-10 16:36:12 +0500

    1

--- McProtoNet.Protocol754/Packets/Client/Game/ClientWindowActionPacket.cs
+++ McProtoNet.Protocol754/Packets/Client/Game/ClientWindowActionPacket.cs
@@ -4,10 +4,20 @@ namespace McProtoNet.Protocol754.Packets.Client
     [PacketInfo(0x09, PacketCategory.Game, 754, PacketSide.Client)]
     public sealed class ClientWindowActionPacket : Packet
     {
-
+        public byte WindowId { get; set; }
+        public short Slot { get; set; }
+        public short ActionId { get; set; }
+        // public WindowActionParam MyProperty { get;  set; }
 
         public override void Write(IMinecraftPrimitiveWriter stream)
         {
+            stream.WriteUnsignedByte(WindowId);
+            stream.WriteShort(Slot);
+            stream.WriteByte(0);
+            stream.WriteShort(ActionId);
+            stream.WriteByte(0);
+
+            stream.WriteByte(0);
 
         }
         public override void Read(IMinecraftPrimitiveReader stream)
