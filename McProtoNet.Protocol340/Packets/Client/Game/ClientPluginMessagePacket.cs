0558cad5a634b737a52ebf8e54d378279122e748
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-10-01 16:23:12 +0500

    PluginMessage 1.12.2

--- McProtoNet.Protocol340/Packets/Client/Game/ClientPluginMessagePacket.cs
+++ McProtoNet.Protocol340/Packets/Client/Game/ClientPluginMessagePacket.cs
@@ -5,6 +5,20 @@ namespace McProtoNet.Protocol340.Packets.Client.Game
 
     public sealed class ClientPluginMessagePacket : Packet
     {
+        public string Channel { get; set; }
+        public byte[] Data { get; set; }
+
+        public ClientPluginMessagePacket()
+        {
+
+        }
+
+        public ClientPluginMessagePacket(string channel, byte[] data)
+        {
+            Channel = channel;
+            Data = data;
+        }
+
         public override void Read(IMinecraftPrimitiveReader stream)
         {
 
@@ -14,7 +28,8 @@ namespace McProtoNet.Protocol340.Packets.Client.Game
         //out.writeBytes(this.data);
         public override void Write(IMinecraftPrimitiveWriter stream)
         {
-
+            stream.WriteString(Channel);
+            stream.Write(Data);
         }
     }
 }
--- McProtoNet.Protocol340/Packets/Server/Game/ServerPluginMessagePacket.cs
+++ McProtoNet.Protocol340/Packets/Server/Game/ServerPluginMessagePacket.cs
@@ -4,11 +4,26 @@ namespace McProtoNet.Protocol340.Packets.Server
 
     public sealed class ServerPluginMessagePacket : Packet
     {
+        public string Channel { get; set; }
+        public byte[] Data { get; set; }
+        public ServerPluginMessagePacket()
+        {
+
+        }
+
+        public ServerPluginMessagePacket(string channel, byte[] data)
+        {
+            Channel = channel;
+            Data = data;
+        }
+
+
         //this.channel = in.readString();
         //this.data = in.readBytes(in.available());
         public override void Read(IMinecraftPrimitiveReader stream)
         {
-
+            Channel = stream.ReadString();
+            Data = stream.ReadToEnd();
         }
 
         public override void Write(IMinecraftPrimitiveWriter stream)
@@ -16,7 +31,7 @@ namespace McProtoNet.Protocol340.Packets.Server
 
         }
 
-        public ServerPluginMessagePacket() { }
+       
     }
 
 }
