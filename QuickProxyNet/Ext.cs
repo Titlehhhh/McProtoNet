8375899bcf3f49f153110450c749b2c38addb7c2
TitleHHHH <TitleHHHH@outlook.com>
2023-07-24 19:10:14 +0500

    Proxy changing

--- QuickProxyNet/Ext.cs
+++ QuickProxyNet/Ext.cs
@@ -1,9 +1,24 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace QuickProxyNet
 {
-    public static class Ext
+	public static class Ext
     {
+
+        public static NetworkCredential CreateFrom(this ProxyCredential proxyCredential)
+        {
+            return new NetworkCredential(proxyCredential.Login, proxyCredential.Password);
+        }
+
+        private static ProxyClientFactory clientFactory = new();
+        public static IProxyClient CreateClient(this ProxyInfo proxy)
+        {
+            if (proxy.Credential is null)
+                return clientFactory.Create(proxy.Type, proxy.Host, proxy.Port);
+            return clientFactory.Create(proxy.Type, proxy.Host, proxy.Port, proxy.Credential.CreateFrom());
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
         public static async ValueTask<int> ReadToEndAsync(this Stream stream, Memory<byte> buffer, int length, CancellationToken token)
         {
