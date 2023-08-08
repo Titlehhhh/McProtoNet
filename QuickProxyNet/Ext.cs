6a2b8ca0c8aa63671c4c28ff8dc93dacb70e4f31
TitleHHHH <TitleHHHH@outlook.com>
2023-08-08 14:25:46 +0500

    1

--- QuickProxyNet/Ext.cs
+++ QuickProxyNet/Ext.cs
@@ -6,19 +6,6 @@ namespace QuickProxyNet
 	public static class Ext
     {
 
-        public static NetworkCredential CreateFrom(this ProxyCredential proxyCredential)
-        {
-            return new NetworkCredential(proxyCredential.Login, proxyCredential.Password);
-        }
-
-        private static ProxyClientFactory clientFactory = new();
-        public static IProxyClient CreateClient(this ProxyInfo proxy)
-        {
-            if (proxy.Credential is null)
-                return clientFactory.Create(proxy.Type, proxy.Host, proxy.Port);
-            return clientFactory.Create(proxy.Type, proxy.Host, proxy.Port, proxy.Credential.CreateFrom());
-        }
-
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
         public static async ValueTask<int> ReadToEndAsync(this Stream stream, Memory<byte> buffer, int length, CancellationToken token)
         {
