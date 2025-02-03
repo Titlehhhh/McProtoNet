d29135b03a2ac2337e301f666505ad0f13d49750
TitleHHHH <TitleHHHH@outlook.com>
2025-02-03 16:08:50 +0500

    add chunk load packet

---
+++ src/McProtoNet.Protocol/ChunkBlockEntity.cs
@@ -0,0 +1,21 @@
+﻿using McProtoNet.NBT;
+
+namespace McProtoNet.Protocol;
+
+public readonly struct ChunkBlockEntity
+{
+    public readonly byte X;
+    public readonly byte Z;
+    public readonly short Y;
+    public readonly int Type;
+    public readonly NbtTag? NbtData;
+
+    public ChunkBlockEntity(byte x, byte z, short y, int type, NbtTag? nbtData)
+    {
+        X = x;
+        Z = z;
+        Y = y;
+        Type = type;
+        NbtData = nbtData;
+    }
+}
---
+++ src/McProtoNet.Protocol/Packets/Play/Clientbound/Manual/MapChunkPacket.cs
@@ -0,0 +1,248 @@
+﻿using McProtoNet.NBT;
+using McProtoNet.Serialization;
+
+namespace McProtoNet.Protocol.Packets.Play.Clientbound;
+
+[PacketInfo("MapChunk",PacketState.Play, PacketDirection.Clientbound)]
+public abstract partial class MapChunkPacket : IServerPacket
+{
+    public int X { get; set; }
+    public int Z { get; set; }
+
+    public byte[] ChunkData { get; set; }
+
+    [PacketSubInfo(340, 404)]
+    public sealed partial class V340_404 : MapChunkPacket
+    {
+        public bool GroundUp { get; set; }
+        public int BitMap { get; set; }
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            GroundUp = reader.ReadBoolean();
+            BitMap = reader.ReadVarInt();
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+    [PacketSubInfo(477, 498)]
+    public sealed partial class V477_498 : MapChunkPacket
+    {
+        public bool GroundUp { get; set; }
+        public int BitMap { get; set; }
+        public NbtTag Heightmaps { get; set; }
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            GroundUp = reader.ReadBoolean();
+            BitMap = reader.ReadVarInt();
+            Heightmaps = reader.ReadNbtTag(true);
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+    [PacketSubInfo(573, 710)]
+    public sealed partial class V573_710 : MapChunkPacket
+    {
+        public bool GroundUp { get; set; }
+        public int BitMap { get; set; }
+        public NbtTag Heightmaps { get; set; }
+
+        public int[]? Biomes { get; set; }
+
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            GroundUp = reader.ReadBoolean();
+            BitMap = reader.ReadVarInt();
+            Heightmaps = reader.ReadNbtTag(true);
+            if (GroundUp)
+            {
+                Biomes = reader.ReadArrayInt32BigEndian(1024);
+            }
+
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+    [PacketSubInfo(734, 736)]
+    public sealed partial class V734_736 : MapChunkPacket
+    {
+        public bool GroundUp { get; set; }
+        public bool IgnoreOldData { get; set; }
+        public int BitMap { get; set; }
+        public NbtTag Heightmaps { get; set; }
+        public int[]? Biomes { get; set; }
+
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            GroundUp = reader.ReadBoolean();
+            IgnoreOldData = reader.ReadBoolean();
+            BitMap = reader.ReadVarInt();
+            Heightmaps = reader.ReadNbtTag(true);
+            if (GroundUp)
+            {
+                Biomes = reader.ReadArrayInt32BigEndian(1024);
+            }
+
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+    [PacketSubInfo(751, 754)]
+    public sealed partial class V751_754 : MapChunkPacket
+    {
+        public bool GroundUp { get; set; }
+        public int BitMap { get; set; }
+        public NbtTag Heightmaps { get; set; }
+        public int[]? Biomes { get; set; }
+
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            GroundUp = reader.ReadBoolean();
+            BitMap = reader.ReadVarInt();
+            Heightmaps = reader.ReadNbtTag(true);
+            if (GroundUp)
+            {
+                Biomes = reader.ReadArray(LengthFormat.VarInt, ReadDelegates.VarInt);
+            }
+
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+
+    [PacketSubInfo(755, 756)]
+    public sealed partial class V755_756 : MapChunkPacket
+    {
+        public long[] BitMap { get; set; }
+        public NbtTag Heightmaps { get; set; }
+        public int[]? Biomes { get; set; }
+
+        public NbtTag[] BlockEntities { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            BitMap = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            Heightmaps = reader.ReadNbtTag(true);
+
+            Biomes = reader.ReadArray(LengthFormat.VarInt, ReadDelegates.VarInt);
+
+
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadNbtTag(true));
+        }
+    }
+
+    [PacketSubInfo(757, 762)]
+    public sealed partial class V757_762 : MapChunkPacket
+    {
+        public NbtTag Heightmaps { get; set; }
+        public ChunkBlockEntity[] BlockEntities { get; set; }
+
+        public bool TrustEdges { get; set; }
+        public long[] SkyLightMask { get; set; }
+        public long[] BlockLightMask { get; set; }
+        public long[] EmptySkyLightMask { get; set; }
+        public long[] EmptyBlockLightMask { get; set; }
+
+        public byte[][] SkyLight { get; set; }
+        public byte[][] BlockLight { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            Heightmaps = reader.ReadNbtTag(true);
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadChunkBlockEntity(protocolVersion));
+
+            TrustEdges = reader.ReadBoolean();
+            SkyLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            BlockLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            EmptySkyLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            EmptyBlockLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            SkyLight = reader.ReadArray(LengthFormat.VarInt, (ref MinecraftPrimitiveReader primitiveReader) =>
+                primitiveReader.ReadArray(LengthFormat.VarInt, ReadDelegates.Byte)
+            );
+            BlockLight = reader.ReadArray(LengthFormat.VarInt, (ref MinecraftPrimitiveReader primitiveReader) =>
+                primitiveReader.ReadArray(LengthFormat.VarInt, ReadDelegates.Byte)
+            );
+        }
+    }
+    //public class V765 Ignore 
+    
+    [PacketSubInfo(763, 769)]
+    public sealed partial class V763_769 : MapChunkPacket
+    {
+        public NbtTag Heightmaps { get; set; }
+        public ChunkBlockEntity[] BlockEntities { get; set; }
+
+        public long[] SkyLightMask { get; set; }
+        public long[] BlockLightMask { get; set; }
+        public long[] EmptySkyLightMask { get; set; }
+        public long[] EmptyBlockLightMask { get; set; }
+
+        public byte[][] SkyLight { get; set; }
+        public byte[][] BlockLight { get; set; }
+
+        public override void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion)
+        {
+            X = reader.ReadSignedInt();
+            Z = reader.ReadSignedInt();
+            Heightmaps = reader.ReadNbtTag(false);
+            ChunkData = reader.ReadBuffer(LengthFormat.VarInt);
+
+            BlockEntities = reader.ReadArray(LengthFormat.VarInt,
+                (ref MinecraftPrimitiveReader r1) => r1.ReadChunkBlockEntity(protocolVersion));
+
+            
+            SkyLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            BlockLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            EmptySkyLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            EmptyBlockLightMask = reader.ReadArrayInt64BigEndian(reader.ReadVarInt());
+            SkyLight = reader.ReadArray(LengthFormat.VarInt, (ref MinecraftPrimitiveReader primitiveReader) =>
+                primitiveReader.ReadArray(LengthFormat.VarInt, ReadDelegates.Byte)
+            );
+            BlockLight = reader.ReadArray(LengthFormat.VarInt, (ref MinecraftPrimitiveReader primitiveReader) =>
+                primitiveReader.ReadArray(LengthFormat.VarInt, ReadDelegates.Byte)
+            );
+        }
+    }
+
+
+
+    public abstract void Deserialize(ref MinecraftPrimitiveReader reader, int protocolVersion);
+}
