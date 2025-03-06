fe3a21a4241c9825fb96760b0ac63e014655ddcd
TitleHHHH <TitleHHHH@outlook.com>
2025-03-06 21:56:09 +0500

    add readme and fix CI

--- build/Build.cs
+++ build/Build.cs
@@ -6,20 +6,28 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [NuGetPackage("Meziantou.Framework.NuGetPackageValidation.Tool",
+        "Meziantou.Framework.NuGetPackageValidation.Tool.dll", Framework = "net8.0")]
+    Tool ValidationTool;
+
+    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json";
+    [Parameter] string NugetApiKey;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath NugetDirectory => ArtifactsDirectory / "nuget";
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -28,11 +36,53 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            ArtifactsDirectory.DeleteDirectory();
+            NugetDirectory.DeleteDirectory();
+            DotNetClean(s => s.SetProject(Solution));
+        });
+
+    Target Validation => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            NugetDirectory.GlobFiles("*.nupkg")
+                .ForEach(x =>
+                {
+                    ValidationTool.Invoke(x.ToString());
+                });
+        });
+
+
+    Target Pack => _ => _
+        .DependsOn(Clean)
+        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Executes(() =>
+        {
+            PackCore(Solution.src.McProtoNet_NBT);
+            PackCore(Solution.src.McProtoNet_Utils);
+            PackCore(Solution.src.McProtoNet_Serialization);
+            PackCore(Solution.src.McProtoNet_Abstractions);
+            PackCore(Solution.src.McProtoNet);
+            PackCore(Solution.src.McProtoNet_Protocol);
+            return;
+
+            void PackCore(Project project)
+            {
+                DotNetPack(s => s
+                    .SetProject(project)
+                    .SetConfiguration(Configuration)
+                    .SetNoDependencies(true)
+                    .SetContinuousIntegrationBuild(true)
+                    .SetOutputDirectory(NugetDirectory));
+            }
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
@@ -40,5 +90,4 @@ class Build : NukeBuild
         .Executes(() =>
         {
         });
-
 }
