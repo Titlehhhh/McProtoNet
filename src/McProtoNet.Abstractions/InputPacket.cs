58a5254f46df980ac199e8ee9ab36aba92cb9ff5
Titlehhhh <aleksejsockov92105@gmail.com>
2025-03-07 12:39:22 +0500

    docs McProtoNet.Abstractions

--- src/McProtoNet.Abstractions/InputPacket.cs
+++ src/McProtoNet.Abstractions/InputPacket.cs
@@ -3,56 +3,60 @@ using System.Runtime.InteropServices;
 using DotNext.Buffers;
 using McProtoNet.Serialization;
 
-namespace McProtoNet.Abstractions;
-
-/// <summary>
-/// 
-/// </summary>
-[StructLayout(LayoutKind.Auto)]
-public struct InputPacket : IDisposable
+namespace McProtoNet.Abstractions
 {
-    public readonly int Id;
-    public readonly ReadOnlyMemory<byte> Data;
-
-
-    private MemoryOwner<byte> owner;
-    private readonly bool isMemoryPool;
-
     /// <summary>
-    /// 
+    /// Represents an input packet received from the Minecraft protocol.
+    /// This structure holds the packet ID and its associated data.
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="owner"></param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public InputPacket(int id, MemoryOwner<byte> owner)
+    [StructLayout(LayoutKind.Auto)]
+    public struct InputPacket : IDisposable
     {
-        this.owner = owner;
-        Id = id;
-        Data = this.owner.Memory;
-       
-       
-    }
+        /// <summary>
+        /// Gets the unique identifier of the packet.
+        /// </summary>
+        public readonly int Id;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="owner"></param>
-    /// <param name="offset"></param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public InputPacket(MemoryOwner<byte> owner, int offset = 0)
-    {
-        this.owner = owner;
-        Memory<byte> mainData = this.owner.Memory.Slice(offset);
+        /// <summary>
+        /// Gets the read-only memory containing the packet data.
+        /// </summary>
+        public readonly ReadOnlyMemory<byte> Data;
 
-        Id = Extensions.ReadVarInt(mainData.Span, out var offsetId);
-        Data = mainData.Slice(offsetId);
-      
-    }
+        private MemoryOwner<byte> owner;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputPacket"/> struct with a specified packet ID and memory owner.
+        /// </summary>
+        /// <param name="id">The unique identifier of the packet.</param>
+        /// <param name="owner">The memory owner containing the packet data.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public InputPacket(int id, MemoryOwner<byte> owner)
+        {
+            this.owner = owner;
+            Id = id;
+            Data = this.owner.Memory;
+        }
 
-    public void Dispose()
-    {
-        if (isMemoryPool)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputPacket"/> struct by extracting the packet ID from the provided memory.
+        /// </summary>
+        /// <param name="owner">The memory owner containing the packet data.</param>
+        /// <param name="offset">The offset in memory from where the packet data begins. Default is 0.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public InputPacket(MemoryOwner<byte> owner, int offset = 0)
+        {
+            this.owner = owner;
+            Memory<byte> mainData = this.owner.Memory.Slice(offset);
+            Id = Extensions.ReadVarInt(mainData.Span, out var offsetId);
+            Data = mainData.Slice(offsetId);
+        }
+
+        /// <summary>
+        /// Releases the resources used by this packet.
+        /// </summary>
+        public void Dispose()
+        {
             owner.Dispose();
+        }
     }
 }
