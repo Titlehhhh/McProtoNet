745fc540d96e309d521809eade803621cc2d877b
TitleHHHH <TitleHHHH@outlook.com>
2025-12-15 18:28:59 +0500

    add types and read methods for testing generator

--- src/McProtoNet.Protocol/Extensions/GenericExtensions.cs
+++ src/McProtoNet.Protocol/Extensions/GenericExtensions.cs
@@ -13,7 +13,6 @@ public static class GenericExtensions
 
         public void WriteBuffer(ReadOnlySpan<byte> buff, int length)
         {
-            
         }
 
         public void WriteBuffer<TLen>(ReadOnlySpan<byte> buff)
@@ -24,12 +23,23 @@ public static class GenericExtensions
                 writer.WriteBuffer(buff);
             }
         }
-        
-        
+
+
         public void WriteType<T>(T val)
         {
-               
         }
     }
-    
+
+    extension(ref MinecraftPrimitiveReader reader)
+    {
+        public Position ReadPosition(int protocolVersion)
+        {
+            ThrowHelper.ThrowIfProtocolNotSupported<Position>(protocolVersion);
+            var locEncoded = reader.ReadSignedLong();
+            var x = (int)(locEncoded >> 38);
+            var z = (int)((locEncoded >> 12) & 0x3FFFFFF);
+            var y = (int)(locEncoded & 0xFFF);
+            return new Position(x, y, z);
+        }
+    }
 }
---
+++ src/McProtoNet.Protocol/Position.cs
@@ -0,0 +1,6 @@
+﻿using McProtoNet.Protocol.Attributes;
+
+namespace McProtoNet.Protocol;
+
+[ProtocolSupport(MinecraftVersion.StartProtocol, MinecraftVersion.LatestProtocol)]
+public partial record struct Position(int X, int Y, int Z);
