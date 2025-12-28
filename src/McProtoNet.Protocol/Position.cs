f42f7a8e6afe8c269ba044dc23c28b07f34b69d4
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2025-12-28 18:06:17 +0500

    Move Position type into Types

--- src/McProtoNet.Protocol/Position.cs
+++
@@ -1,6 +0,0 @@
-﻿using McProtoNet.Protocol.Attributes;
-
-namespace McProtoNet.Protocol;
-
-[ProtocolSupport(MinecraftVersion.StartProtocol, MinecraftVersion.LatestProtocol)]
-public partial record struct Position(int X, int Y, int Z);
